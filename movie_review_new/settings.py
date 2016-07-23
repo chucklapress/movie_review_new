e7692325b00d5319b9a7fa6a57ed7666fd0a3815
chucklapress <charleslapress@gmail.com>
2016-07-22 21:26:11 -0400

    added data migration for rater

--- movie_review_new/settings.py
+++ movie_review_new/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'movie_review_new.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
