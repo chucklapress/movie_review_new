3a42b236217ae7d07054c409d96b07c329928182
chucklapress <charleslapress@gmail.com>
2017-07-19 16:05:53 -0400

    added host for ngrok

--- movie_review_new/settings.py
+++ movie_review_new/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p=%$&t99r^vhx_9(*3-b)885j1_gz^=^@n60lt(=u=c1mad97k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
